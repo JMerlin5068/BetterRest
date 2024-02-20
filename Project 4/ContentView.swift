47dce6e0ecd0c5474ea130856361bee259b4b29c
Jarrod Merlin <jarrod.merlin@stu.barren.kyschools.us>
2024-02-20 08:22:50 -0600

    Initial Commit

---
+++ Project 4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project 4
+//
+//  Created by Student on 2/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project 4/Project_4App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_4App.swift
+//  Project 4
+//
+//  Created by Student on 2/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Project_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
